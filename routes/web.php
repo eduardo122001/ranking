bc0cfc74384e98a2103515fd38fe70d32b501471
JIMBOB-cool <matias.sanchez@ucsp.edu.pe>
2026-05-19 15:24:14 -0500

    Agregado login con Google

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\GoogleController;
+use App\Http\Controllers\NotaController;
 
 Route::get('/', function () {
     return view('welcome');
